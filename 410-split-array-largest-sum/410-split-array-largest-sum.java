cbb3ca1a26d61bfda25b6de8c39f141f38c822ee
Nikhil soni <1802127.cse.cec@cgc.edu.in>
2022-05-21 13:33:21 +0530

    Time: 2 ms (39.08%), Space: 41.3 MB (75.86%) - LeetHub

---
+++ 410-split-array-largest-sum/410-split-array-largest-sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int splitArray(int[] nums, int m) {
+        int start = 0, end = 0;
+        for(int num: nums) {
+            start = Math.max(start, num);
+            end += num;
+        }
+        
+        while(start < end) {
+            int mid = start + (end - start) / 2;
+            
+            int sum = 0, pieces = 1;
+            for(int num: nums) {
+                if(sum + num > mid) {
+                    sum = num;
+                    pieces++;
+                } else {
+                    sum += num;
+                }
+            }
+            if(pieces > m) start = mid + 1;
+            else end = mid;
+        }
+        return end; 
+    }
+}
